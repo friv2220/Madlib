da8f60fc17816f4b8c7e845d5a840ff87624a02a
pknowledge <hayk.grigoryan@Hayks-MacBook-Air.local>
2024-04-21 13:45:49 +0400

    first commit

---
+++ Madlib.py
@@ -0,0 +1,12 @@
+adj_1 = input("Adjective: ")
+adj_2 = input("Adjective: ")
+adj_3 = input("Adjective: ")
+adj_4 = input("Adjective: ")
+verb_1=input("Verb: ")
+verb_2=input("Verb: ")
+verb_3=input("Verb: ")
+
+
+madlib = f"The {adj_1} sun {verb_1} the vibrant meadow. A {adj_2} breeze whispered through the air, {verb_2} the {adj_3} scent of blooming flowers. Life thrived in this {adj_4} oasis, where nature's beauty {verb_3} all who beheld it.!"
+
+print(madlib)
